5e5c4664b75131d3904821402844a457f6a09f9b
David Disu <disudavid9@gmail.com>
2025-04-29 09:18:01 -0500

    Step 1 completed, Home Screen

---
+++ TrackingApp/src/main/java/com/pluralsight/TrackingApp.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class TrackingApp {
+
+    public static void main(String[] args) {
+
+        //Create a home screen that takes user input
+
+        System.out.println("Welcome to TrackingApp Home Screen!");
+
+        boolean running = true;
+        // Create identifiers and options for the home screen
+        while (running) {
+            System.out.println("D - Add Deposit");
+            System.out.println("P - Make Payment");
+            System.out.println("L - Display Ledger");
+            System.out.println("X - Exit Application");
+            System.out.print("Select your option: ");
+            Scanner scanner = new Scanner(System.in);
+            String userInput = scanner.next();
+
+            //Create loop that'll prompt users input
+
+
+            if (userInput.equalsIgnoreCase("D")) {
+                System.out.println("Would you like add deposit information?");
+                break;
+            } else if (userInput.equalsIgnoreCase("P")) {
+                System.out.println("Would you like to make a payment through use of debit?");
+                break;
+            }
+            if (userInput.equalsIgnoreCase("L")) {
+                System.out.println("Would like to display Ledger Screen?");
+                break;
+            }
+            if (userInput.equalsIgnoreCase("X")) {
+                System.out.println("Goodbye User!");
+                break;
+            } else ;
+            {
+                System.out.println("Invalid input. Try Again");
+            }
+
+            scanner.close();
+
+        }
+    }
+}
